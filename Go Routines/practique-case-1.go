814aaa25d2ac058e65bc9308d322d06632729be6
Ignacio Falco <ignacio.falco17@gmail.com>
2021-11-11 15:44:26 -0300

    caso practico

---
+++ Go Routines/practique-case-1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func procesoQuePuedeTardarMucho(nombre string) <-chan int {
+	r := make(chan int)
+
+	go func() {
+		// aca simulamos un proceso que puede ser lento y de tiempo variable
+		// como una consulta HTTP a una API, un query a una base de datos
+		// lectura de un archivo en disco, etc
+
+		inicio := time.Now().UnixMilli()                                // inicio y fin son usados para medir el tiempo de ejecucion
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(5*1000))) // esperamos un tiempo aleatorio, entre 0 y 5 segundos
+		fin := time.Now().UnixMilli()
+		fmt.Printf("%s duró %d milisegundos\n", nombre, fin-inicio)
+
+		r <- rand.Intn(100)
+	}()
+
+	return r
+}
+
+func sumar(a, b int) int {
+	fmt.Printf("a vale %d, y b vale %d\n", a, b)
+	return a + b
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	inicio := time.Now().UnixMilli()
+	a, b := procesoQuePuedeTardarMucho("Tarea A"), procesoQuePuedeTardarMucho("Tarea B")
+	fmt.Printf("El resultado de la suma es: %d\n", sumar(<-a, <-b))
+	fin := time.Now().UnixMilli()
+	fmt.Printf("Toda la ejecucion duró %d milisegundos\n", fin-inicio)
+}
